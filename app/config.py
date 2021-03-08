fbd44053cca0e8a2bd68f5aa8d0d35a16cf65518
Joanna-Joy Gray <jjgray21@hotmail.com>
2021-03-07 23:26:46 -0500

    Setup db connection

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'some$3cretKey'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://lab5:lab5@localhost/lab5'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
 class DevelopmentConfig(Config):
